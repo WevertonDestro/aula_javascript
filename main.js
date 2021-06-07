a05713ff03ff6eef31a554bbcca820692a580c54
Weverton Fernando Destro <85394571+WevertonDestro@users.noreply.github.com>
2021-06-07 17:14:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+};
+
+function redirecionar(){
+    window.open("https://www.instagram.com/");
+};
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+   
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(10, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+//console.log("Hoje é" + d.getDate() + "/"+ d.getMonth()+1);
+
+/*
+var d = new Date();
+alert(d.getMonth());
+alert(d.getMinutes());
+alert(d.getHours());
+*/
+
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count)
+}
+*/
+
+
+/*
+var count = 0;
+
+while (count < 5){
+    console.log(count);
+    count = count + 1;
+}
+*/
+
+
+/*
+var idade = prompt("Digite sua idade: ");
+
+if (idade >= 18){
+    alert("Maior de idade.");
+}else{
+    alert("Menor de idade");
+};
+*/
+
+//var frutas = [{nome:"maçã", cor:"vermelho"}, {nome:"uva", cor:"roxa"}];
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+//var fruta = {nome:"maçã", cor:"vermelho"};
+//console.log(fruta.nome);
+
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+//var nome = "Weverton Destro";
+//var idade = 32;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade2 + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
